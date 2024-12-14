5d2d936d1ffebeff11546400b7f699b4cdb772f1
xhj <838158472@qq.com>
2024-12-14 18:04:32 +0800

    修改 wel

--- home/src/main/java/com/xy/home/page/act/WelcomeAct.kt
+++ home/src/main/java/com/xy/home/page/act/WelcomeAct.kt
@@ -3,6 +3,8 @@ package com.xy.home.page.act
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import com.gyf.immersionbar.BarHide
+import com.gyf.immersionbar.ktx.immersionBar
 import com.xy.home.R
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.Job
@@ -19,13 +21,18 @@ class WelcomeAct : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wel)
+        immersionBar {
+            hideBar(BarHide.FLAG_HIDE_BAR)
+            transparentBar()
+            fitsSystemWindows(false)
+        }
         gotoMainPage()
     }
 
     private fun gotoMainPage() {
         //携程 非阻塞式
         jumpJob = GlobalScope.launch {
-            Thread.sleep(100)
+            Thread.sleep(5000)
             startActivity(Intent(this@WelcomeAct, MainActivity::class.java))
             finish()
         }
