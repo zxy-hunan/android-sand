695dd2ae7578c0a33fbb4a8e53e6957ecdbcfaa3
xhj <838158472@qq.com>
2025-06-27 15:40:39 +0800

    修改themes

---
+++ home/src/main/java/com/xy/home/page/act/TestAcy.kt
@@ -0,0 +1,18 @@
+package com.xy.home.page.act
+
+import android.app.Activity
+import android.os.Bundle
+import com.xy.home.R
+
+/**
+ * @file TestAcy
+ * @author zxy
+ * @date 2025/6/26 9:57
+ * @brief test
+ */
+class TestAcy :Activity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+    }
+}
--- home/src/main/java/com/xy/home/page/act/WelcomeAct.kt
+++ home/src/main/java/com/xy/home/page/act/WelcomeAct.kt
@@ -1,9 +1,20 @@
 package com.xy.home.page.act
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import android.app.Activity
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.DecelerateInterpolator
+import android.view.animation.OvershootInterpolator
 import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.core.animation.doOnEnd
+import com.google.android.material.animation.AnimatorSetCompat.playTogether
 import com.gyf.immersionbar.BarHide
 import com.gyf.immersionbar.ktx.immersionBar
 import com.xy.common.data.AppFontsType
@@ -22,6 +33,7 @@ import kotlinx.coroutines.launch
  */
 class WelcomeAct : Activity() {
     lateinit var jumpJob: Job
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wel)
@@ -33,13 +45,56 @@ class WelcomeAct : Activity() {
         var tvSlogen = findViewById<TextView>(R.id.tv_slogen)
         AppFontsUtil.setControlFonts(mContext, tvSlogen, AppFontsType.BARLOW_SEMI_BOLD)
         gotoMainPage()
+
+        /*splashScreen.setOnExitAnimationListener { splashScreenViewProvider ->
+            splashScreenViewProvider.iconView?.let {
+                showSplashIconExitAnimator(it) {
+                    splashScreenViewProvider.remove()
+                }
+            }
+        }*/
+    }
+
+
+
+    private fun showSplashIconExitAnimator(iconView: View, onExit: () -> Unit = {}) {
+        val alphaOut = ObjectAnimator.ofFloat(iconView, "alpha", 1f, 0f).apply {
+            duration = 300 // 动画时长 300ms
+            interpolator = AccelerateInterpolator() // 加速效果
+        }
+
+        val scaleOut = ObjectAnimator.ofPropertyValuesHolder(
+            iconView,
+            PropertyValuesHolder.ofFloat("scaleX", 1f, 0f), // X轴缩放
+            PropertyValuesHolder.ofFloat("scaleY", 1f, 0f)  // Y轴缩放
+        ).apply {
+            duration = 400
+            interpolator = OvershootInterpolator() // 弹性效果
+        }
+
+        val slideUp = ObjectAnimator.ofFloat(
+            iconView,
+            "translationY",
+            0f,
+            -iconView.height.toFloat() // 向上滑动一个视图高度
+        ).apply {
+            duration = 500
+            interpolator = DecelerateInterpolator() // 减速效果
+        }
+
+
+        AnimatorSet().run {
+            playTogether(alphaOut, scaleOut, slideUp)
+            doOnEnd { onExit() }
+        }
     }
 
     private fun gotoMainPage() {
         //携程 非阻塞式
         jumpJob = GlobalScope.launch {
             Thread.sleep(3000)
-            startActivity(Intent(this@WelcomeAct, MainActivity::class.java))
+            startActivity(Intent(this@WelcomeAct, TestAcy::class.java))
+//            startActivity(Intent(this@WelcomeAct, MainActivity::class.java))
             finish()
         }
 
